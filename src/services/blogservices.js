125abdbf6e3cc3e221f443826e01ba4830f7355a
bgjedlik <bolya.gabor@jedlik.eu>
2023-12-11 12:26:18 +0100

    newblog

--- src/services/blogservices.js
+++ src/services/blogservices.js
@@ -9,6 +9,16 @@ export default {
             .catch(err => {
                 return Promise.reject(err.response);
             })
+    },
+
+    insertBlog(data,token){
+        return Axios.post('/blog',data,{headers: {"Authorization": "Bearer " + token}})
+            .then(resp =>{
+                return resp.data;
+            })
+            .catch(err =>{
+                return Promise.reject(err.response);
+            })
     }
 
 }
--- src/views/blog/BlogsView.vue
+++ src/views/blog/BlogsView.vue
@@ -3,9 +3,15 @@
         <h1 class="display-5 mt-5">Blogok</h1>
         <div class="row">
             <div class="col-12" v-for="blog in blogs">
-                <h3>{{ blog.title }}</h3>
+                <!-- <h3>{{ blog.title }}</h3>
                 <p><strong>Dátum:</strong> {{ blog.updated_at }}, <strong>Szerző: </strong>{{ blog.user_name  }} </p>
-                <p>{{ blog.description }}</p>
+                <p>{{ blog.description }}</p> -->
+                <div v-if="blog.user_id == user.id || user.role == 1">
+                    <AdminBlog :blog="blog"/>
+                </div>
+                <div v-else>
+                    <ShowBlog :blog="blog"/>
+                </div>
                 <hr>
             </div>
         </div>
@@ -15,6 +21,12 @@
 <script setup>
 import { ref } from 'vue';
 import blogservices from '../../services/blogservices';
+import ShowBlog from '../../components/blog/ShowBlog.vue';
+import AdminBlog from '../../components/blog/AdminBlog.vue';
+import { useUserStore } from '../../stores/userstore';
+import {storeToRefs} from 'pinia';
+
+const { user } = storeToRefs( useUserStore() );
 
 const blogs = ref();
 
